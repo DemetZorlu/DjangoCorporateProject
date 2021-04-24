bb6cabc35e94d60c92e5353c22d1165d5fe8b26a
demetzorlu <demetzorlu23@gmail.com>
2021-04-25 01:01:44 +0300

    15 Django E ticaret Kategori alt kategori menu Geliştirmeleri

--- home/views.py
+++ home/views.py
@@ -3,14 +3,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from content.models import Content
+from content.models import Content, Menu
 from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
     setting = Setting.objects.all()
     sliderdata = Content.objects.filter(type=4, status="True")
-    context = {'setting': setting[0], 'page': 'home', 'sliderdata': sliderdata}
+    menu = Menu.objects.filter(status="True")
+    context = {'menu': menu, 'setting': setting[0], 'page': 'home', 'sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
 
@@ -30,23 +31,27 @@ def contact(request):
 
     setting = Setting.objects.all()
     form = ContactFormu()
-    context = {'setting': setting[0], 'page': 'contact', 'pagename': 'İletişim', 'form': form}
+    menu = Menu.objects.filter(status="True")
+    context = {'menu': menu, 'setting': setting[0], 'page': 'contact', 'pagename': 'İletişim', 'form': form}
     return render(request, 'contact.html', context)
 
 
 def about(request):
     setting = Setting.objects.all()
-    context = {'setting': setting[0], 'page': 'about', 'pagename': 'Hakkımızda'}
+    menu = Menu.objects.filter(status="True")
+    context = {'menu': menu, 'setting': setting[0], 'page': 'about', 'pagename': 'Hakkımızda'}
     return render(request, 'about.html', context)
 
 
 def referances(request):
     setting = Setting.objects.all()
-    context = {'setting': setting[0], 'page': 'referances', 'pagename': 'Referanslarımız'}
+    menu = Menu.objects.filter(status="True")
+    context = {'menu': menu, 'setting': setting[0], 'page': 'referances', 'pagename': 'Referanslarımız'}
     return render(request, 'referances.html', context)
 
 
 def academiccalendar(request):
     setting = Setting.objects.all()
-    context = {'setting': setting[0], 'page': 'academiccalendar', 'pagename': 'Akademik Takvim'}
+    menu = Menu.objects.filter(status="True")
+    context = {'menu': menu, 'setting': setting[0], 'page': 'academiccalendar', 'pagename': 'Akademik Takvim'}
     return render(request, 'academiccalendar.html', context)
